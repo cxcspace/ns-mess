46468889dcf4f3ec98f73f2f1fec8a75fb22112e
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-11-21 19:49:07 -0800

    first pass at demonstrating failures

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 )
 
 func mainWithErr() error {
-	log.Println(Snap("main start"))
+	originalMain := Snap("main start")
+	log.Println(originalMain)
 
 	// create a new namespace
 	newNS, err := ns.NewNS()
@@ -24,13 +25,38 @@ func mainWithErr() error {
 		}
 	}()
 
+	makeMeSomeGoRoutines := make(chan string)
+	stopTheGoRoutines := make(chan struct{})
+
+	// in the background, we spin up some goroutines
+	go func() {
+		for name := range makeMeSomeGoRoutines {
+			myName := name
+			go func() {
+				originalGoRoutine := Snap(myName)
+				reportIfNamespaceSwitch(myName, "start", originalMain, originalGoRoutine)
+				log.Println(originalGoRoutine)
+				<-stopTheGoRoutines
+				finalGoRoutine := Snap(myName)
+				reportIfNamespaceSwitch(myName, "end", originalGoRoutine, finalGoRoutine)
+			}()
+		}
+	}()
+
 	// spawn a goroutine that sits in that namespace
 	err = newNS.Do(func(prevNS ns.NetNS) error {
-		log.Println(Snap("newns goroutine"))
+		originalNewNS := Snap("newns start")
+		log.Println(originalNewNS)
+		for i := 0; i < 25; i++ {
+			makeMeSomeGoRoutines <- fmt.Sprintf("goroutine %2d", i)
+		}
+		close(stopTheGoRoutines)
+		finalNewNS := Snap("newns stop")
+		reportIfNamespaceSwitch("newns", "end", originalNewNS, finalNewNS)
 		return nil
 	})
 
-	log.Println(Snap("main end"))
+	log.Println(Snap("main stop "))
 	return err
 }
 
@@ -42,8 +68,18 @@ func Snap(name string) Snapshot {
 	}
 }
 
+func reportIfNamespaceSwitch(name, state string, original, final Snapshot) {
+	if final.NS != original.NS {
+		log.Printf("%25s: %5s: expected to be in NS %s but am instead in NS %s", name, state, original.NS, final.NS)
+	}
+}
+
 func (s Snapshot) String() string {
-	return fmt.Sprintf("%20s: thread %6s in namespace %10s", s.Name, s.Thread, s.NS)
+	return fmt.Sprintf("%25s: thread %6s in namespace %10s", s.Name, s.Thread, s.NS)
+}
+
+func (s Snapshot) Numbers() string {
+	return fmt.Sprintf("thread %s in namespace %s", s.Thread, s.NS)
 }
 
 type Snapshot struct {
@@ -93,6 +129,7 @@ func getInodeFd(fd int) (uint64, error) {
 }
 
 func main() {
+	log.SetFlags(0)
 	if err := mainWithErr(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
